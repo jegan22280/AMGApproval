5f8490cc34a6f6e6438511311b556fe5fd91123d
jegan22280 <jegan22280@gmail.com>
2023-07-07 15:39:38 -0400

    played with stuff

--- backupDocs.php
+++ backupDocs.php
@@ -2,8 +2,6 @@
 <?php  require_once 'includes/sessions.php';  ?>
 <?php  require_once 'includes/functions.php'; ?>
 <?php  require_once 'PDFMerger/PDFMerger.php'; ?>
-
-
 <?php  use PDFMerger\PDFMerger;  ?>
 <?php
 $_SESSION['Queryparameter'] = $_GET["id"];
@@ -45,30 +43,28 @@ $queryParameter = $_SESSION['Queryparameter'];
       $endPage = $nextRow['PAGE_NUM'];
     }
     
-  
-  if (!isset($endPage)) {
-    $endPage = 128;
-  }
-  
-  
-  if (isset($micro)) {
+    if (!isset($endPage)) {
+      $endPage = 128;
+    }
     
-    $pdf = new PDFMerger;
-    //the micro number needs broken to date and batch. the last chars represent the batch
-    $batch = substr($micro, -1);
-    // this will take the micro and toss the last digit [substr($micro,0,-1)] then force it to me a 6 digit [sprintf("%06d",] number then format it as a date
-    $date = DateTime::createFromFormat('mdY',sprintf("%06d", substr($micro,0,-1)))->format('m-d-y');
     
-    $pathTofile = '\\\\192.168.0.22\\x-drive\\DB\\'.$client.' '.$date.' '.$batch.'.pdf';
-    $filename = $client.' '.$date.' '.$batch.'.pdf';
-
+    if (isset($micro)) {
+      
+      $pdf = new PDFMerger;
+      //the micro number needs broken to date and batch. the last chars represent the batch
+      $batch = substr($micro, -1);
+      // this will take the micro and toss the last digit [substr($micro,0,-1)] then force it to me a 6 digit [sprintf("%06d",] number then format it as a date
+      $date = DateTime::createFromFormat('mdY',sprintf("%06d", substr($micro,0,-1)))->format('m-d-y');
+      
+      $pathTofile = '\\\\192.168.0.22\\x-drive\\DB\\'.$client.' '.$date.' '.$batch.'.pdf';
+      $filename = $client.' '.$date.' '.$batch.'.pdf';
+      
     if (!file_exists($pathTofile)) {
       $_SESSION["errorMessage"] = "Image not available.";
       redirect_to('error.php');
     }
-
-    $pdf->addPDF($pathTofile, $startPage."-".($endPage-1))
-    ->merge('browser', $filename);
+    
+    $pdf->addPDF($pathTofile, $startPage."-".($endPage-1))->merge('browser', $filename);
   //       //REPLACE 'file' WITH 'browser', 'download', 'string', or 'file' for output options
   //     	//You do not need to give a file path for browser, string, or download - just the name.
     
